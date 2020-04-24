ea49386187bfe84c5c15aabfaace65a93c1a4b9b
selja2798 <siotong2798@gmail.com>
2020-04-24 23:56:44 +0700

    Updates

---
+++ precache-manifest.a640b4df292f994240142c8ffecb1550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e40c76f488825cbd8c244ae058b791b",
+    "url": "/monster-me/index.html"
+  },
+  {
+    "revision": "6e7dd753f08822d0a659",
+    "url": "/monster-me/static/css/main.8ebafb89.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/monster-me/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-me/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7dd753f08822d0a659",
+    "url": "/monster-me/static/js/main.8e6413b6.chunk.js"
+  },
+  {
+    "revision": "582f3a32f404fd1936e4",
+    "url": "/monster-me/static/js/runtime-main.56f37170.js"
+  }
+]);
