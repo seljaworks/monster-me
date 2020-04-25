19bb227a6849b1c0c7dc6ebdc23240f12585a69f
selja2798 <siotong2798@gmail.com>
2020-04-25 18:07:00 +0700

    fix bug

--- src/components/card-list/cart-list.component.jsx
+++ src/components/card-list/cart-list.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-
-import "./card-list.style.css";
 import Card from "../card/card.component";
 
+// import style from "./card-list.style.css";
+import styles from "./card-list.module.css";
 const CardList = (props) => {
   return (
-    <div className="card-list">
-      {props.monsterList.map((monster) => (
-        <Card monster={monster} key={monster.id} />
-      ))}
+    <div className={props.monsterList.length <= 0 ? "" : styles.cardList}>
+      {props.monsterList.length <= 0 ? (
+        <div className={styles.emptyList}>Can't find monster</div>
+      ) : (
+        props.monsterList.map((monster) => (
+          <Card monster={monster} key={monster.id} />
+        ))
+      )}
     </div>
   );
 };
